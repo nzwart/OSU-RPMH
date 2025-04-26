2973360a1300abf58b4b71db5cbaa592d86c75b4
mattand <mjande@umich.edu>
2025-04-26 14:29:12 -0400

    Abstract led pins into LedArray interface

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 // Import HAL crates
 use rp_pico::entry;
 use rp_pico::hal;
+use rp_pico::hal::gpio::DefaultTypeState;
 use rp_pico::hal::pac;
 use rp_pico::hal::prelude::*;
 
@@ -20,6 +21,32 @@ use cortex_m::delay::Delay;
 use dht20::Dht20;
 
 use panic_halt as _;
+
+struct LedArray {
+    led_pin_yellow:     Pin<hal::gpio::bank0::Gpio14, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
+    led_pin_red:        Pin<hal::gpio::bank0::Gpio15, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
+    led_pin_green:      Pin<hal::gpio::bank0::Gpio16, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
+    led_pin_yellow2:    Pin<hal::gpio::bank0::Gpio13, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
+    led_pin_red2:       Pin<hal::gpio::bank0::Gpio12, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
+}
+
+impl LedArray {
+    fn new(
+        gpio12: Pin<hal::gpio::bank0::Gpio12, <hal::gpio::bank0::Gpio12 as DefaultTypeState>::Function, <hal::gpio::bank0::Gpio12 as DefaultTypeState>::PullType>,
+        gpio13: Pin<hal::gpio::bank0::Gpio13, <hal::gpio::bank0::Gpio13 as DefaultTypeState>::Function, <hal::gpio::bank0::Gpio13 as DefaultTypeState>::PullType >,
+        gpio14: Pin<hal::gpio::bank0::Gpio14, <hal::gpio::bank0::Gpio14 as DefaultTypeState>::Function, <hal::gpio::bank0::Gpio14 as DefaultTypeState>::PullType>,
+        gpio15: Pin<hal::gpio::bank0::Gpio15, <hal::gpio::bank0::Gpio15 as DefaultTypeState>::Function, <hal::gpio::bank0::Gpio15 as DefaultTypeState>::PullType>,
+        gpio16: Pin<hal::gpio::bank0::Gpio16, <hal::gpio::bank0::Gpio16 as DefaultTypeState>::Function, <hal::gpio::bank0::Gpio16 as DefaultTypeState>::PullType>,
+    ) -> Self {
+        LedArray {
+            led_pin_yellow: gpio14.into_push_pull_output(),
+            led_pin_red: gpio15.into_push_pull_output(),
+            led_pin_green: gpio16.into_push_pull_output(),
+            led_pin_yellow2: gpio13.into_push_pull_output(),
+            led_pin_red2: gpio12.into_push_pull_output(),
+        }
+    }
+}
 
 // Abstract the components we'll be using on the board into their own struct
 // This is useful for passing around the components in a single "object"
@@ -40,13 +67,10 @@ struct BoardComponents {
     // LED Outputs
     // note: we're using PullDown to match what into_push_pull_output() returns, as we need to explicitly specify all generic type parameters in Rust struct definitions. The into_push_pull_output() function configures pins with PullDown by default, so by using the same type here, we ensure compatibility between our struct definition and the initialization code in setup_board()
     led_pin_led: Pin<hal::gpio::bank0::Gpio25, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>, // note: this is the onboard LED, whereas the others in the following initializations are LEDs physically connected to GPIO pins
-    led_pin_yellow:
-        Pin<hal::gpio::bank0::Gpio14, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
-    led_pin_red: Pin<hal::gpio::bank0::Gpio15, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
-    led_pin_green: Pin<hal::gpio::bank0::Gpio16, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
-    led_pin_yellow2:
-        Pin<hal::gpio::bank0::Gpio13, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
-    led_pin_red2: Pin<hal::gpio::bank0::Gpio12, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
+
+    // A struct containing all indicator LEDs and methods to control their behavior
+    led_array: LedArray,
+
     // todo: Other peripherals can be added below, such as an LCD
 }
 
@@ -90,12 +114,8 @@ fn setup_board() -> BoardComponents {
 
     // Set the onboard RPP LED to be an output
     let led_pin_led = pins.led.into_push_pull_output();
-    // Set the GPIO 14, 15, 16 (Pico pins 19, 20, 21) to be an output
-    let led_pin_yellow = pins.gpio14.into_push_pull_output();
-    let led_pin_red = pins.gpio15.into_push_pull_output();
-    let led_pin_green = pins.gpio16.into_push_pull_output();
-    let led_pin_yellow2 = pins.gpio13.into_push_pull_output();
-    let led_pin_red2 = pins.gpio12.into_push_pull_output();
+
+    let led_array = LedArray::new(pins.gpio12, pins.gpio13, pins.gpio14, pins.gpio15, pins.gpio16);
 
     // Configure two pins as being I²C, not GPIO
     let sda_pin = pins.gpio18.reconfigure();
@@ -121,11 +141,7 @@ fn setup_board() -> BoardComponents {
     BoardComponents {
         sensor,
         led_pin_led,
-        led_pin_yellow,
-        led_pin_red,
-        led_pin_green,
-        led_pin_yellow2,
-        led_pin_red2,
+        led_array,
     }
 }
 
@@ -138,21 +154,21 @@ fn main() -> ! {
     // sensor.read will produce two f32 values: reading.hum and reading.temp
     match components.sensor.read() {
         Ok(reading) => {
-            if reading.hum > 0.0 {
-                components.led_pin_red.set_high().unwrap();
-            }
-            if reading.hum > 20.0 {
-                components.led_pin_yellow.set_high().unwrap();
-            }
-            if reading.hum > 40.0 {
-                components.led_pin_green.set_high().unwrap();
-            }
-            if reading.hum > 60.0 {
-                components.led_pin_yellow2.set_high().unwrap();
-            }
-            if reading.hum > 80.0 {
-                components.led_pin_red2.set_high().unwrap();
-            }
+//             if reading.hum > 0.0 {
+                // components.led_pin_red.set_high().unwrap();
+            // }
+            // if reading.hum > 20.0 {
+                // components.led_pin_yellow.set_high().unwrap();
+            // }
+            // if reading.hum > 40.0 {
+                // components.led_pin_green.set_high().unwrap();
+            // }
+            // if reading.hum > 60.0 {
+                // components.led_pin_yellow2.set_high().unwrap();
+            // }
+            // if reading.hum > 80.0 {
+                // components.led_pin_red2.set_high().unwrap();
+            // }
         }
         Err(_e) => {
             components.led_pin_led.set_high().unwrap();
