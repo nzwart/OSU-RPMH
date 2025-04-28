01164366e0bf589b68aa70fd1a7b00fd4ccba6f2
mattand <mjande@umich.edu>
2025-04-28 18:59:44 -0400

    Minor fixes from checking on breadboard

--- src/bin/led_test.rs
+++ src/bin/led_test.rs
@@ -2,27 +2,57 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::delay::Delay;
+
 // Import HAL crates
 use rp_pico::entry;
 use rp_pico::hal;
 use rp_pico::hal::pac;
+use rp_pico::hal::gpio::Pin;
+
+// HAL traits
+use embedded_hal::digital::OutputPin;
 
 use dht20::Reading;
 
 use panic_halt as _;
 
+use rp_pico::hal::Clock;
 use OSU_RPMH::leds;
 
 // A pared down version of BoardComponents for this test executable
 struct BoardComponents {
     // A struct containing all indicator LEDs and methods to control their behavior
     led_array: leds::LedArray,
+
+    led_pin_led: Pin<hal::gpio::bank0::Gpio25, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
+    delay: Delay,
 }
 
 // Set up all of our board components and return them in a single struct
 fn setup_board() -> BoardComponents {
     // This is the Pico-specific setup
     let mut peripherals = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(peripherals.WATCHDOG);
+
+    // Configure the clocks
+    // (The default is to generate a 125 MHz system clock)
+    let clocks = hal::clocks::init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
+        peripherals.XOSC,
+        peripherals.CLOCKS,
+        peripherals.PLL_SYS,
+        peripherals.PLL_USB,
+        &mut peripherals.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let delay = Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(peripherals.SIO);
@@ -35,12 +65,16 @@ fn setup_board() -> BoardComponents {
         &mut peripherals.RESETS,
     );
 
+    let led_pin_led = pins.led.into_push_pull_output();
+
     // Initialize an led array with five led pins
     let led_array = leds::LedArray::new(pins.gpio12, pins.gpio13, pins.gpio14, pins.gpio15, pins.gpio16);
 
     // Return all components in the form of the struct (LCD will need to be added here as well)
     BoardComponents {
+        led_pin_led,
         led_array,
+        delay,
     }
 }
 
@@ -61,10 +95,21 @@ fn main() -> ! {
     let mut i = 0;
 
     loop {
+        components.led_pin_led.set_high().unwrap();
+        components.delay.delay_ms(500);       
+        components.led_pin_led.set_low().unwrap();
+
+        components.led_array.clear();
+        components.delay.delay_ms(500);
+ 
         if i < readings.len() {
             components.led_array.update(readings[i].clone());
             i += 1;
+        } else {
+            i = 0;
         }
+
+        components.delay.delay_ms(500);
     }
 
 }
--- src/leds.rs
+++ src/leds.rs
@@ -4,8 +4,8 @@ use dht20::Reading;
 
 // A struct for interacting with the LED Array 
 pub struct LedArray {
-    led_pin_yellow:     Pin<Gpio14, gpio::FunctionSioOutput, gpio::PullDown>,
     led_pin_red:        Pin<Gpio15, gpio::FunctionSioOutput, gpio::PullDown>,
+    led_pin_yellow:     Pin<Gpio14, gpio::FunctionSioOutput, gpio::PullDown>,
     led_pin_green:      Pin<Gpio16, gpio::FunctionSioOutput, gpio::PullDown>,
     led_pin_yellow2:    Pin<Gpio13, gpio::FunctionSioOutput, gpio::PullDown>,
     led_pin_red2:       Pin<Gpio12, gpio::FunctionSioOutput, gpio::PullDown>,
@@ -21,28 +21,37 @@ impl LedArray {
         gpio16: Pin<Gpio16, <Gpio16 as DefaultTypeState>::Function, <Gpio16 as DefaultTypeState>::PullType>,
     ) -> Self {
         LedArray {
-            led_pin_yellow: gpio14.into_push_pull_output(),
             led_pin_red: gpio15.into_push_pull_output(),
+            led_pin_yellow: gpio14.into_push_pull_output(),
             led_pin_green: gpio16.into_push_pull_output(),
             led_pin_yellow2: gpio13.into_push_pull_output(),
             led_pin_red2: gpio12.into_push_pull_output(),
         }
     }
 
+    // Turn off all leds
+    pub fn clear(&mut self) {
+        self.led_pin_red.set_low().unwrap();
+        self.led_pin_yellow.set_low().unwrap();
+        self.led_pin_green.set_low().unwrap();
+        self.led_pin_yellow2.set_low().unwrap();
+        self.led_pin_red2.set_low().unwrap();
+    }
+
     // Update the LED array based on a sensor reading
     pub fn update(&mut self, reading: Reading) {
         if reading.hum > 0.0 {
             self.led_pin_red.set_high().unwrap();
-        }
+        } 
         if reading.hum > 20.0 {
-            self.led_pin_yellow.set_high().unwrap();
-        }
+            self.led_pin_yellow.set_high().unwrap();    
+        } 
         if reading.hum > 40.0 {
             self.led_pin_green.set_high().unwrap();
-        }
+        } 
         if reading.hum > 60.0 {
             self.led_pin_yellow2.set_high().unwrap();
-        }
+        } 
         if reading.hum > 80.0 {
             self.led_pin_red2.set_high().unwrap();
         }
