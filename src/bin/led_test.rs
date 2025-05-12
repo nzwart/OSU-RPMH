cd82f2fa33369c1ffebec1384b9c145f8a1082c3
mattand <mjande@umich.edu>
2025-05-12 13:17:46 -0400

    Refactor led_test and add test for sensor

--- src/bin/led_test.rs
+++ src/bin/led_test.rs
@@ -2,92 +2,19 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::delay::Delay;
-
-// Import HAL crates
 use rp_pico::entry;
-use rp_pico::hal;
-use rp_pico::hal::gpio::Pin;
-use rp_pico::hal::pac;
-
-// HAL traits
 use embedded_hal::digital::v2::OutputPin;
-
 use OSU_RPMH::dht::Reading;
-
 use panic_halt as _;
-
-use rp_pico::hal::Clock;
-use OSU_RPMH::leds;
-
-// A pared down version of BoardComponents for this test executable
-struct BoardComponents {
-    // A struct containing all indicator LEDs and methods to control their behavior
-    led_array: leds::LedArray,
-
-    led_pin_led: Pin<hal::gpio::bank0::Gpio25, hal::gpio::FunctionSioOutput, hal::gpio::PullDown>,
-    delay: Delay,
-}
-
-// Set up all of our board components and return them in a single struct
-fn setup_board() -> BoardComponents {
-    // This is the Pico-specific setup
-    let mut peripherals = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(peripherals.WATCHDOG);
-
-    // Configure the clocks
-    // (The default is to generate a 125 MHz system clock)
-    let clocks = hal::clocks::init_clocks_and_plls(
-        rp_pico::XOSC_CRYSTAL_FREQ,
-        peripherals.XOSC,
-        peripherals.CLOCKS,
-        peripherals.PLL_SYS,
-        peripherals.PLL_USB,
-        &mut peripherals.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let delay = Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(peripherals.SIO);
-
-    // Set the pins up according to their function on this particular board
-    let pins = rp_pico::Pins::new(
-        peripherals.IO_BANK0,
-        peripherals.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut peripherals.RESETS,
-    );
-
-    let led_pin_led = pins.led.into_push_pull_output();
-
-    // Initialize an led array with five led pins
-    let led_array = leds::LedArray::new(
-        pins.gpio12,
-        pins.gpio13,
-        pins.gpio14,
-        pins.gpio15,
-        pins.gpio16,
-    );
-
-    // Return all components in the form of the struct (LCD will need to be added here as well)
-    BoardComponents {
-        led_pin_led,
-        led_array,
-        delay,
-    }
-}
+use OSU_RPMH::board;
 
 /*
+    Tests the functionality of 5 light LED array. If operating normally, the LEDs
+    will blink in sequence, starting with just one LED, then two, three, four, and 
+    finally all five LEDs. This is meant to simulate the different levels of 
+    humidity that can be read by the sensor.
+    
     To run the test program, use the command $cargo run --bin led_test
-
-    TODO: Move the above documentation into a more obvious place (possibly the README)
 */
 #[entry]
 fn main() -> ! {
@@ -116,17 +43,17 @@ fn main() -> ! {
     ];
 
     // Set up the board and get all components via our struct
-    let mut components = setup_board();
+    let mut components = board::BoardComponents::setup_board();
 
     let mut i = 0;
 
     loop {
         components.led_pin_led.set_high().unwrap();
-        components.delay.delay_ms(500);
+        components.sensor.delay_ms(500);
         components.led_pin_led.set_low().unwrap();
 
         components.led_array.clear();
-        components.delay.delay_ms(500);
+        components.sensor.delay_ms(500);
 
         if i < readings.len() {
             components.led_array.update(readings[i].clone());
@@ -135,6 +62,6 @@ fn main() -> ! {
             i = 0;
         }
 
-        components.delay.delay_ms(500);
+        components.sensor.delay_ms(500);
     }
 }
---
+++ src/bin/sensor_test.rs
@@ -0,0 +1,53 @@
+// Compile without standard library
+#![no_std]
+#![no_main]
+
+use rp_pico::entry;
+use embedded_hal::digital::v2::OutputPin;
+use OSU_RPMH::board::BoardComponents;
+use panic_halt as _;
+use OSU_RPMH::board;
+
+// Helper function: blinks the on-board LED n times
+fn blink(components: &mut BoardComponents, n: u32) {
+    for _ in 0..n {
+        components.led_pin_led.set_high().unwrap();
+        components.sensor.delay_ms(500);
+        components.led_pin_led.set_low().unwrap();
+        components.sensor.delay_ms(500);
+    }
+}
+
+/*
+    Tests the functionality of the DHT20 humidity sensor. If operating normally, the LED indicator on
+    the Raspberry Pi Pico chip will blink once and then rest for 15s. Repeated blinking or a solid light
+    for 15s indicates incorrect component state.
+    
+    To run the test program, use the command $cargo run --sensor_test
+*/
+#[entry]
+fn main() -> ! {
+
+    // Set up the board and get all components via our struct
+    let mut components = board::BoardComponents::setup_board();
+
+    loop {
+        components.led_pin_led.set_low().unwrap();
+
+        match components.sensor.read() {
+            // "Good" reading: blink once
+            Ok(reading) if reading.hum > 10.0 && reading.hum < 90.0 => blink(&mut components, 1),
+            // Unusually low reading: blink twice
+            Ok(reading) if reading.hum < 10.0 => blink(&mut components, 2),
+            // Unusually high reading: blink 3 times
+            Ok(reading) if reading.hum > 90.0 => blink(&mut components, 3),
+            // Reading that is outside normal range but did not generate an error: blink 4 times
+            Ok(_) => blink(&mut components, 4),
+            // Error reading: solid LED indicator
+            Err(_) => components.led_pin_led.set_high().unwrap(),
+        }
+
+        // Wait 15s before next reading
+        components.sensor.delay_ms(15000);
+    }
+}
