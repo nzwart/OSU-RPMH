746652c1129c5df1f16f13508ff6abd538eb0b9b
mattand <mjande@umich.edu>
2025-05-26 15:19:36 -0400

    Update led_test and sensor test to match board setup changes

--- src/bin/sensor_test.rs
+++ src/bin/sensor_test.rs
@@ -4,17 +4,20 @@
 
 use rp_pico::entry;
 use embedded_hal::digital::v2::OutputPin;
-use OSU_RPMH::{board::BoardComponents, pico};
+use OSU_RPMH::{board::BoardComponents, pico, shared_delay::{self, DelayTimer}};
 use panic_halt as _;
 use OSU_RPMH::board;
 
+use embedded_hal::blocking::delay::DelayMs;
+
 // Helper function: blinks the on-board LED n times
-fn blink(components: &mut BoardComponents, n: u32) {
+fn blink(components: &mut BoardComponents, n: u32, delay: &mut DelayTimer) {
     for _ in 0..n {
         components.led_pin_led.set_high().unwrap();
-        components.sensor.delay_ms(500);
+        delay.delay_ms(500);
+
         components.led_pin_led.set_low().unwrap();
-        components.sensor.delay_ms(500);
+        delay.delay_ms(500);
     }
 }
 
@@ -30,26 +33,35 @@ fn main() -> ! {
 
     let mut rpp_core = pico::CoreComponents::setup_board();
 
+    let mut delays = shared_delay::Delays::new(&rpp_core.shared_timer);
+
     // Set up the board and get all components via our struct
-    let mut components = board::BoardComponents::setup_board(&mut rpp_core.delay, rpp_core.i2c, rpp_core.led_pin_led, rpp_core.led_array);
+    let mut components = board::BoardComponents::setup_board(
+        &rpp_core.shared_timer, 
+        rpp_core.sensor_i2c, 
+        &mut rpp_core.i2clcd,
+        &mut delays.lcd_delay,
+        rpp_core.led_pin_led, 
+        rpp_core.led_array
+    );
 
     loop {
         components.led_pin_led.set_low().unwrap();
 
         match components.sensor.read() {
             // "Good" reading: blink once
-            Ok(reading) if reading.hum > 10.0 && reading.hum < 90.0 => blink(&mut components, 1),
+            Ok(reading) if reading.hum > 10.0 && reading.hum < 90.0 => blink(&mut components, 1, &mut delays.generic_delay),
             // Unusually low reading: blink twice
-            Ok(reading) if reading.hum < 10.0 => blink(&mut components, 2),
+            Ok(reading) if reading.hum < 10.0 => blink(&mut components, 2, &mut delays.generic_delay),
             // Unusually high reading: blink 3 times
-            Ok(reading) if reading.hum > 90.0 => blink(&mut components, 3),
+            Ok(reading) if reading.hum > 90.0 => blink(&mut components, 3, &mut delays.generic_delay),
             // Reading that is outside normal range but did not generate an error: blink 4 times
-            Ok(_) => blink(&mut components, 4),
+            Ok(_) => blink(&mut components, 4, &mut delays.generic_delay),
             // Error reading: solid LED indicator
             Err(_) => components.led_pin_led.set_high().unwrap(),
         }
 
         // Wait 15s before next reading
-        components.sensor.delay_ms(15000);
+        delays.generic_delay.delay_ms(15000);
     }
 }
